db75b217533239d744043e885760c50344337aa1
Iris <iris.thuo@student.moringaschool.com>
2022-11-25 15:24:03 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide= function(){
+    let num1=2000;
+    let num2=100;
+    return num1/num2;
+}
+//arrow function square
+const square = (num3) => num3*num3;
+
+//arrow function add
+const add= (num4,num5)=> num4+num5;
